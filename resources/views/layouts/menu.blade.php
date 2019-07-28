fe2ff0f9e9701a458dd56e19b091c825a6a20277
fahmikudo <fahmi.hidayatullah12@gmail.com>
2019-07-29 03:12:41 +0700

    add session for user

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,15 +4,21 @@
             <li>
                 <a href="{{ route('home') }}"><i class="fa fa-users fa-fw"></i> Profile </a>
             </li>
+            @if (Auth::user()->jabatan == 'ADMIN')
             <li>
                 <a href="{{ route('pegawai-index') }}"><i class="fa fa-bars fa-fw"></i> Pegawai </a>
             </li>
+            @endif
+            @if (Auth::user()->jabatan == 'ADMIN')
             <li>
                 <a href="{{ route('barang-index') }}"><i class="fa fa-bars fa-fw"></i> Barang </a>
             </li>
+            @endif
+            @if (Auth::user()->jabatan == 'ADMIN')
             <li>
                 <a href="{{ route('jenispelayanan-index') }}"><i class="fa fa-bars fa-fw"></i> Jenis Pelayanan </a>
             </li>
+            @endif
             <li>
                 <a href="{{ route('pelanggan-index') }}"><i class="fa fa-bars fa-fw"></i> Pelanggan </a>
             </li>
